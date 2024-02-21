1b1ab1afff1d364e5cf8895120685aade286c8f7
Milo Galli <milogalli@Milos-MBP-2.wind3.hub>
2024-02-21 23:17:34 +0100

    fixed a bit the project layout

---
+++ Mandelbrot/mandelbrot_lab/target/debug/deps/mandelbrot_lab-2e2c3b39414384fe.d
@@ -0,0 +1,6 @@
+/Users/milogalli/Desktop/Milo/university/DOING/HPC-UNIGE/Mandelbrot/mandelbrot_lab/target/debug/deps/mandelbrot_lab-2e2c3b39414384fe: src/main.rs src/mandelbrot_frame.rs
+
+/Users/milogalli/Desktop/Milo/university/DOING/HPC-UNIGE/Mandelbrot/mandelbrot_lab/target/debug/deps/mandelbrot_lab-2e2c3b39414384fe.d: src/main.rs src/mandelbrot_frame.rs
+
+src/main.rs:
+src/mandelbrot_frame.rs:
