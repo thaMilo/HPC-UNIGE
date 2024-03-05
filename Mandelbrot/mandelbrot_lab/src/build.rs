a5011d200c74a43a9832031641e055d8a2422387
Giorgio Rengucci <37120983+GiorgioRen@users.noreply.github.com>
2024-03-05 15:24:30 +0100

    Add build stage

---
+++ Mandelbrot/mandelbrot_lab/src/build.rs
@@ -0,0 +1,29 @@
+use std::process::Command;
+use std::path::Path;
+use std::fs;
+
+fn main() {
+	let src_dir = Path::new("src/mandelbrot/metal");
+    let metal_path = src_dir.join("mandelbrot.metal");
+    let metallib_path = src_dir.join("mandelbrot.metallib");
+	let air_path = src_dir.join("mandelbrot.air");
+
+    // Only compile if the .metal file is newer than the .metallib file
+    if metallib_path.exists() && fs::metadata(metal_path.clone()).unwrap().modified().unwrap() <= fs::metadata(metallib_path.clone()).unwrap().modified().unwrap() {
+        return;
+    }
+
+    // Compile the .metal file to a .metallib file
+    Command::new("xcrun")
+        .args(&["metal", "-c", metal_path.to_str().unwrap(), "-o", air_path.to_str().unwrap()])
+        .status()
+        .unwrap();
+
+    Command::new("xcrun")
+        .args(&["metallib", air_path.to_str().unwrap(), "-o", metallib_path.to_str().unwrap()])
+        .status()
+        .unwrap();
+
+    // Tell Cargo to rerun this script if the .metal file has changed
+    println!("cargo:rerun-if-changed={}", metal_path.to_str().unwrap());
+}
